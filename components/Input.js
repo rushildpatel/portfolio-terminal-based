9453f6909db02931a72f690b75ecfa26d1859f55
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-06-09 16:00:50 -0400

    Created Input component to input commands

---
+++ components/Input.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./Input.module.css";
+
+export default function Input({ command, onSubmit }) {
+  const [_command, setCommand] = useState(command ? command : "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCommand("");
+    return onSubmit(_command);
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
+      <label htmlFor="command">
+        <span style={{ color: "#ff9e64" }}>λ</span> ::
+        {"~"}
+        <span style={{ color: "var(--primary)" }}>~</span>
+        {"~"}
+        <span style={{ color: "var(--secondary)" }}>&gt;&gt;</span>
+      </label>
+
+      <input type="text" className={styles.input} value={_command} onChange={(e) => setCommand(e.target.value)} disabled={command ? true : false} ref={(input) => input && !command && input.focus()} autoFocus={command === ""}></input>
+    </form>
+  );
+}
