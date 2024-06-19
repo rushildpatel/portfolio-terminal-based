800342cc6fb4dbcb128857c40669e6d3671a029a
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-06-19 19:03:35 -0400

    created home page which renders the terminal component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
+import Terminal from "@/components/Terminal";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <div className={styles.container}>
+      <h1>
+        rushil_patel:$ <span className={style.help}> type "help" to start</span>
+      </h1>
+      <p>
+        Visit{" "}
+        <a href="https://rushilpatel.me/" target="_blank" rel="noreferrer">
+          NORMAL WEBSITE
+        </a>
+      </p>
+      <Terminal />
+    </div>
+  );
 }
