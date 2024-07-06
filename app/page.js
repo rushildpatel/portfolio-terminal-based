3cba98a92223ba805005ed6b8d25b8dc88c67ea4
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-07-05 21:19:24 -0400

    updated theme colors

--- app/page.js
+++ app/page.js
@@ -4,15 +4,20 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
+      <div>
+        <a className={styles.button} href="https://rushilpatel.me/" target="_blank">
+          Simplified Portfolio View [↗]
+        </a>
+      </div>
       <h1>
-        rushil_patel:$ <span className={styles.help}> type "help" to start</span>
+        rushil_patel:$ <span className={styles.help}> type <strong style={{color: "white"}}>"help"</strong> to start</span>
       </h1>
-      <p>
+      {/* <p>
         Visit{" "}
         <a href="https://rushilpatel.me/" target="_blank" rel="noreferrer">
           SIMPLIFIED PORTFOLIO VIEW
         </a>
-      </p>
+      </p> */}
       <Terminal />
     </div>
   );
